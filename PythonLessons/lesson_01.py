68f41f81e6806094f0d2b143a67584f06bc762f1
tariq <m.tariq.sarfraz.007@gmail.com>
2024-06-01 21:21:01 +0500

    Done Lesson 1

---
+++ PythonLessons/lesson_01.py
@@ -0,0 +1,39 @@
+#part 1
+
+# num_1 = 5
+# num_2 = 6
+
+# print("num_ 1 : " , num_1)
+# print("num_ 2 : " , num_2)
+
+# num_2 = num_1 + num_2
+
+# num_1 = num_1 + num_2 
+
+
+
+#________________________________________
+
+# part 2
+
+def add_numbers(num_1 , num_2=0):
+    result = num_1 + num_2
+    print(result)
+
+def multiply_numbers(num_1 , num_2):
+    result = num_1 * num_2
+    print(result)
+
+
+
+
+
+num_1 = 5
+num_2 = 6
+add_numbers(num_1 , num_2)
+multiply_numbers(num_1 , num_2)
+
+
+
+
+# Assignment Add , Multiply , Subtract , Divide , Print_number 
